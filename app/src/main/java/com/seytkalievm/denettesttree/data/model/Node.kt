806cc0946e7995d200de774f5ac42444490ec750
seytkalievm <3meuka0@gmail.com>
2023-03-02 10:33:17 +0300

    refactor node

--- app/src/main/java/com/seytkalievm/denettesttree/data/model/Node.kt
+++ app/src/main/java/com/seytkalievm/denettesttree/data/model/Node.kt
@@ -1,15 +1,14 @@
 package com.seytkalievm.denettesttree.data.model
 
 class Node (
-    val parent: Node? = null,
+    private val _parent: Node? = null,
     private val _children: MutableList<Node> = mutableListOf()
 ) {
 
     val children get() = _children
+    val parent get() = _parent
 
-    fun addChild() {
-        _children.add(Node(parent = this))
-    }
+    fun addChild() = _children.add(Node(_parent = this))
 
     fun deleteChild(node: Node) = _children.remove(node)
 
@@ -17,6 +16,5 @@ class Node (
     Take last 20 bytes of the hashCode
      */
     fun getName() = this.hashCode().toString(2).takeLast(20)
-
 
 }
